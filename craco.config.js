84b869b2f5792e30420e4cd97ee1ad7c3ca1fdc8
abdelatief <abdelatief99@gmail.com>
2022-01-26 22:51:59 +0200

    initial commit, prettier, husky, craco config

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const CracoAlias = require('craco-alias')
+
+module.exports = {
+	plugins: [
+		{
+			plugin: CracoAlias,
+			options: {
+				source: 'jsconfig',
+				baseUrl: './src',		// baseUrl SHOULD be specified, plugin does not take it from tsconfig
+			},
+		},
+	],
+}
