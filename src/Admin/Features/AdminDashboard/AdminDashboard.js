dbd7c10fa9dd89cce3fcce16be06167baf80226a
Shahd <Shahdhosny58@gmail.com>
2022-01-28 06:24:41 +0200

    start admin dashboard

--- src/Admin/Features/AdminDashboard/AdminDashboard.js
+++ src/Admin/Features/AdminDashboard/AdminDashboard.js
@@ -1,11 +1,29 @@
 import Layout from '@Admin/Containers/Layout/Layout'
+import styled from 'styled-components'
+import { Text } from '@Components'
 
 const AdminDashboard = () => {
 	return (
-		<div>
+		<StyledDiv>
 			<Layout />
-		</div>
+			<StyledContainer>
+				<Text fontFamily='Jost-Bold' fontSize={5}>
+					Analytics Overview
+				</Text>
+			</StyledContainer>
+		</StyledDiv>
 	)
 }
+const StyledDiv = styled.div`
+	display: block;
+	position: relative;
+`
+const StyledContainer = styled.div`
+	display: flex;
+	position: absolute;
+	margin: 20px;
+	left: 200px;
+	top: 100px;
+`
 
 export default AdminDashboard
