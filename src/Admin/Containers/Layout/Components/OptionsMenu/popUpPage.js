3b793cefce48dda5fa0ee49c35006ae643a09082
Shahd <Shahdhosny58@gmail.com>
2022-02-04 02:24:05 +0200

    change popUp top value

--- src/Admin/Containers/Layout/Components/OptionsMenu/popUpPage.js
+++ src/Admin/Containers/Layout/Components/OptionsMenu/popUpPage.js
@@ -17,7 +17,7 @@ const PopUpPage = ({ open, children, onClose }) => {
 const OverlayStyles = styled.div`
 	display: flex;
 	position: fixed;
-	top: 5%;
+	top: 60px;
 	left: 0;
 	right: 0;
 	bottom: 0;
@@ -25,6 +25,9 @@ const OverlayStyles = styled.div`
 	z-index: 1000;
 	@media (min-width: 1000px) {
 		display: none;
+	}
+	@media (max-width: 450px) {
+		top: 80px;
 	}
 `
 const CloseIcon = styled(AiOutlineClose)`
