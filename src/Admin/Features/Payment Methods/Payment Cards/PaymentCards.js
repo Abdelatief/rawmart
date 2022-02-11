5535c53f7f97a64dca933815773433100e147bb6
Shahd <Shahdhosny58@gmail.com>
2022-02-11 22:15:12 +0200

    create cards files

---
+++ src/Admin/Features/Payment Methods/Payment Cards/PaymentCards.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PaymentCards = () => {
+	return <div>PaymentCards</div>
+}
+
+export default PaymentCards
---
+++ src/Admin/Features/Payment Methods/Payment Cards/PaymentsData.js
@@ -0,0 +1,26 @@
+export const PaymentsData = [
+	{
+		id: 1,
+		name: 'Visa',
+		description: 'Description test',
+		active: true,
+		credentials: [
+			{
+				key: 1,
+				value: 'value 1',
+			},
+		],
+	},
+	{
+		id: 2,
+		name: 'Cash',
+		description: 'Description test',
+		active: true,
+		credentials: [
+			{
+				key: 1,
+				value: 'value 1',
+			},
+		],
+	},
+]
